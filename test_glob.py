a2824d80f93ba4c6c869e6324e4c053255655663
tsato <suika.1106@gmail.com>
2018-04-07 11:06:02 +0900

    fixed

--- test_glob.py
+++ test_glob.py
@@ -6,5 +6,7 @@ print(path_list)
 
 """
 $ python test_glob.py 
-['test_sys_args.py', 'test_time.py', 'test_tempfile.py', 'test_multiprocessing.py', 'test_os_walk.py', 'test_bm.py', 'test_prim.py', 'test_glob.py', 'test_argparse.py']
+['test_sys_args.py', 'test_time.py', 'test_tempfile.py', 
+ 'test_multiprocessing.py', 'test_os_walk.py', 'test_bm.py', 
+ 'test_prim.py', 'test_glob.py', 'test_argparse.py']
 """
