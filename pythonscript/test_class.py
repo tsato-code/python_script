7de8e98b2346021ff81d94e1f91ab3ddc67d6be5
tsato <suika.1106@gmail.com>
2018-05-14 16:49:40 +0900

    fixed

--- pythonscript/test_class.py
+++ pythonscript/test_class.py
@@ -1,28 +1,46 @@
 class Manga(object):
-    def __init__(self, title, artist):
+    def __init__(self, index, title, artist, volumes, anime, movie):
+        self.index = index
         self.title = title
         self.artist = artist
+        self.volumes = volumes
+        self.anime = anime
+        self.movie = movie
 
-    def print(self):
-    	print(self.title, '-', self.artist)
+
+    def __repr__(self):
+    	return '<Manga({}): {} - {} - {} - {} - {}>'.format(self.index, self.title, self.artist, self.volumes, self.anime, self.movie)
+
+
+    def add_vol(self):
+    	self.volumes += 1
 
 
 def main():
 	manga_list = []
-	manga_list.append(Manga('Chihayafuru', 'Yuki Suetsugu'))
-	manga_list.append(Manga('Case Closed', 'Gosho Aoyama'))
-	manga_list.append(Manga('Shogi no Watanabe-kun', 'Megumi Ina'))
-	manga_list.append(Manga('Magic Kaito', 'Gosho Aoyama'))
+	manga_list.append(Manga(1, 'Chihayafuru', 'Yuki Suetsugu', 38, True, True))
+	manga_list.append(Manga(2, 'Case Closed', 'Gosho Aoyama', 94, True, True))
+	manga_list.append(Manga(3, 'Shogi no Watanabe-kun', 'Megumi Ina', 3, False, False))
+	manga_list.append(Manga(4, 'Magic Kaito', 'Gosho Aoyama', 5, True, False))
 
 
 	print('\n* manga list *')
 	for manga in manga_list:
-		manga.print()
+		print(manga)
 
-	print('\n* Gosho Aoyama written *')
-	manga_list_filter = filter(lambda manga: manga.artist == 'Gosho Aoyama', manga_list)
+
+	print('\n* Aoyama written *')
+	manga_list_filter = filter(lambda manga: 'Aoyama' in manga.artist, manga_list)
 	for manga in manga_list_filter:
-	    manga.print()
+	    print(manga)
+
+	print('\n* add volume *')
+	manga_list[1].add_vol()
+
+
+	print('\n* check *')
+	print(manga_list[1])
+
 
 
 if __name__ == '__main__':
@@ -34,12 +52,17 @@ if __name__ == '__main__':
 $ python test_class.py 
 
 * manga list *
-Chihayafuru - Yuki Suetsugu
-Case Closed - Gosho Aoyama
-Shogi no Watanabe-kun - Megumi Ina
-Magic Kaito - Gosho Aoyama
+<Manga(1): Chihayafuru - Yuki Suetsugu - 38 - True - True>
+<Manga(2): Case Closed - Gosho Aoyama - 94 - True - True>
+<Manga(3): Shogi no Watanabe-kun - Megumi Ina - 3 - False - False>
+<Manga(4): Magic Kaito - Gosho Aoyama - 5 - True - False>
 
-* Gosho Aoyama written *
-Case Closed - Gosho Aoyama
-Magic Kaito - Gosho Aoyama
+* Aoyama written *
+<Manga(2): Case Closed - Gosho Aoyama - 94 - True - True>
+<Manga(4): Magic Kaito - Gosho Aoyama - 5 - True - False>
+
+* add volume *
+
+* check *
+<Manga(2): Case Closed - Gosho Aoyama - 95 - True - True>
 """
