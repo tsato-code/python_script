8a28d89c6f5c55fbdfb769104a809a5c41f2a787
tsato <suika.1106@gmail.com>
2018-06-01 14:22:51 +0900

    .

--- pythonscript/test_datetime_day_of_week.py
+++ pythonscript/test_datetime_day_of_week.py
@@ -1,7 +1,16 @@
 import datetime
 
-yobi = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+in_datetime = '2018/06/01 22:50:31'
 
-in_date = '2016/07/30'
-a = datetime.datetime.strptime(in_date,'%Y/%m/%d')
-print ("{} {}".format(in_date, yobi[a.weekday()]))
+
+# 曜日計算
+WEEK = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+d = datetime.datetime.strptime(in_datetime,'%Y/%m/%d %H:%M:%S')
+print ("date {} ({})".format(d.date(), WEEK[d.weekday()]))
+
+
+# 時刻加算
+start = datetime.datetime.strptime(in_datetime, '%Y/%m/%d %H:%M:%S')
+end = start + datetime.timedelta(hours=8) 
+print("START {}".format( start.time() ))
+print("  END {}".format( end.time() ))
