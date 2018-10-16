7ad2c77ca28a69ace24fed29e3e3471990ada333
tsato <suika.1106@gmail.com>
2018-10-16 10:37:44 +0900

    カルマンフィルタ

--- machinelearning/kalman_filter.py
+++ machinelearning/kalman_filter.py
@@ -13,13 +13,24 @@ from pykalman import KalmanFilter
 # ------------------
 # 定数パラメータ設定
 # ------------------
-x = -0.37727
+x = 0.0
 transition_mat = np.array([[1, 1], [0, 1]])
-transition_cov = 0.0000001*np.eye(2)
+transition_cov = 0.001*np.eye(2)
+
+
 # ------------------
 # 関数の定義
 # ------------------
-
+def output(kf):	
+	# 出力
+	# print("kf.initial_state_mean: {}".format(kf.initial_state_mean))
+	# print("kf.initial_state_covariance: {}".format(kf.initial_state_covariance))
+	# print("kf.transition_matrices: {}".format(kf.transition_matrices))
+	# print("transition_offsets: {}".format(kf.transition_offsets))
+	# print("transition_covariance: {}".format(kf.transition_covariance))
+	print("observation_matrices: {}".format(kf.observation_matrices))
+	print("observation_offsets: {}".format(kf.observation_offsets))
+	print("observation_covariance: {}".format(kf.observation_covariance))
 
 # ------------------
 # メイン処理
@@ -29,15 +40,30 @@ def main():
 	observations = np.random.normal(x, 1, size=50)
 
 	# 適当な推移行列からカルマンフィルタモデル作成
-	# kf = KalmanFilter(transition_matrices=transition_mat,
-	#                   transition_covariance=transition_cov)
-	kf = KalmanFilter()
+	kf = KalmanFilter(transition_matrices=transition_mat,
+	                  transition_covariance=transition_cov)
+	# kf = KalmanFilter()
 
 	# 推定
-	kf.em(observations,n_iter=20)
+	kf.em(observations, n_iter=5)
 	(filtered_state_means, filtered_state_covariances) = kf.filter(observations)
 	(smoothed_state_means, smoothed_state_covariances) = kf.smooth(observations)
 
+	# 出力
+	output(kf)
+
+	# 更新
+	for obs in observations:
+		(filtered_state_means, filtered_state_covariances) = kf.filter_update(
+			filtered_state_mean=filtered_state_means[-1], filtered_state_covariance=filtered_state_covariances[-1], observation=obs
+		)
+		output(kf)
+
+	print(observations)
+	print(filtered_state_means)
+	print(filtered_state_covariances)
+
+	"""
 	# 可視化
 	plt.figure(figsize=(6,4))
 	plt.plot(observations, "-xc", label="observations")
@@ -47,6 +73,10 @@ def main():
 	plt.legend()
 	plt.show()
 	plt.close()
+	"""
+
+
+
 
 
 
