b34d6e3354c87015d3bb98e80c0080c0cf7624de
tsato <suika.1106@gmail.com>
2018-12-14 13:34:44 +0900

    位置情報データ構造geohash

--- pythonscript/test_geohash2.py
+++ pythonscript/test_geohash2.py
@@ -12,6 +12,8 @@ lat =  35.681166
 lon = 139.766992
 enc = geohash2.encode(lat, lon, precision=8)
 dec = geohash2.decode(enc)
+dec_e = geohash2.decode_exactly(enc)
 
-print ('Geohash for {}, {}: {}'.format(lat, lon, enc))
-print ('Coordinate for Geohash {}: {}'.format(enc, dec))
+print('Geohash for {}, {}: {}'.format(lat, lon, enc))
+print('Coordinate for Geohash {}: {}'.format(enc, dec))
+print('Coordinate for Geohash {}: {}'.format(enc, dec_e))
